8150167afc24da7dea61e50f9140dcbf21a7bc27
tblader <tblader@wisc.edu>
2024-05-21 19:17:01 +0000

    simplified python script

--- lambda_functions/src/s3-bucket-demo.py
+++ lambda_functions/src/s3-bucket-demo.py
@@ -13,32 +13,26 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    logger.info('Starting s3-bucket-demo')
-    print(event)
-    print(context)
-    print("Hello, World!")
     
+    # without a bucket to work with, we can't do anything in this lambda
     if os.environ.get('S3_BUCKET_NAME') is None:
         print("S3_BUCKET_NAME is not set")
         sys.exit(1)
 
+    # get the name of the s3 bucket from the environment
     mybucket = os.environ.get('S3_BUCKET_NAME')
-    logger.info("Bucket name from environment variable: " + mybucket)
 
     # get a list of s3 buckets in the account
     s3 = boto3.client('s3')
     buckets = s3.list_buckets()
-    logger.info("buckets: " + str(buckets['Buckets']))
     
     # print the name of the s3 bucket retreived
     mybucket = buckets['Buckets'][0]['Name']
-    print(f"mybucket: {mybucket}")
 
     # create a file prefixed withe the current time and upload it to the s3 bucket
     current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     src_file = current_time + ".txt"
     s3.put_object(Body=f"Hello from {mybucket} - current time is {current_time}" , Bucket=mybucket, Key=src_file)
-    logger.info('s3-bucket-demo complete')
     
 
 if os.environ.get('DEBUG_MODE') == 'true':
